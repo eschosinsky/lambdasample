161b9f109d54ac9857478fe8ee87f75a8e8dc87d
eschosinsky <eschosinsky@hotmail.com>
2025-08-06 23:41:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
     headers: {
       "Content-Type": "text/plain",
     },
-    body: "Hallo Große Welt",
+    body: "Hallo meine Welt",
   };
   
   return response;

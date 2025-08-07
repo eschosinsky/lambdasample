ccdb5ec3e3b4c8aeb495712b0e1307c72792fc01
Ewald Schosinsky E <eschosinsky@hotmail.com>
2025-08-06 21:12:54 -0600

    carga inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+
+exports.handler = async (event) => {
+  // El "handler" es la función que AWS Lambda ejecutará.
+  // "async" indica que es una función asíncrona, lo cual es una buena práctica.
+  // "event" es el objeto que contiene la información de la solicitud (si aplica).
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: "Hello World",
+  };
+  
+  return response;
+};
